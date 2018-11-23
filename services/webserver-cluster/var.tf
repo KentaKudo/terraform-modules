f007f8bb824a4294356aea8c65314425ba2399a2
KentaKudo <yd37574@gmail.com>
2018-11-23 18:54:18 +0000

    Modify: conditional auto scaling

--- services/webserver-cluster/var.tf
+++ services/webserver-cluster/var.tf
@@ -29,3 +29,7 @@ variable "max_size" {
   description = "The maximum number of EC2 Instances in the ASG"
   default = 10
 }
+
+variable "enable_autoscaling" {
+  description = "If set to true, enable auto scaling"
+}
